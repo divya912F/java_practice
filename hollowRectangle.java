894fec66138d411beb7e32bb5eca854ab3f127b5
Divyanshi Panwar <divyapanwar912@gmail.com>
2026-06-10 12:00:16 +0530

    Add files via upload

---
+++ hollowRectangle.java
@@ -0,0 +1,33 @@
+/*   *****
+     *   *
+     *   *
+     *****
+ */
+// hollow rectangle or square
+
+
+
+import java.util.*;
+public class hollowRectangle {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+
+        int row=sc.nextInt();
+        int column=sc.nextInt();
+
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=column;j++){
+                if(i==1||j==1||i==row||j==column){
+                     System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+
+                }
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+    
+}
